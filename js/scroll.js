5938c75bdf4a27608d5e0d42c48705e2ef8d6851
rheeeuro <eurohand@naver.com>
2021-07-15 10:40:16 +0900

    scroll

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+disableScroll = () => {
+    document.querySelector('body').addEventListener('touchmove', this.removeEvent, { passive: false });
+    document.querySelector('body').addEventListener('onclick', this.removeEvent, { passive: false });
+    document.querySelector('body').addEventListener('mousewheel', this.removeEvent, { passive: false });
+};
+
+disableScroll();
